cd4bd5c05f3ef41bca49e73e8b9f722ba87093a9
aman-maharshi <amanm4harshi@gmail.com>
2020-11-04 23:56:56 +0530

    progress 10/30

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+class NotFound extends React.Component {
+    render() {
+        return <h2 className="not-found-title">Page Not Found</h2>
+    }
+}
+
+export default NotFound
--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
 import "./css/style.css"
+import "./css/custom.css"
+
 import StorePicker from "./components/StorePicker"
 import App from "./components/App"
+import NotFound from "./components/NotFound"
 
-ReactDOM.render(<App />, document.querySelector("#main"))
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={StorePicker} />
+                <Route path="/store/:storeId" component={App} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<Root />, document.querySelector("#main"))
