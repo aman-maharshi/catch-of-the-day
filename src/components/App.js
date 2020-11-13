a54bceaa7333bd9cc4d410e67bb6f4e455c632f0
aman-maharshi <amanm4harshi@gmail.com>
2020-11-13 09:48:11 +0530

    progress 15/30

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./Header"
 import Order from "./Order"
 import Inventory from "./Inventory"
+import Fish from "./Fish"
 
 import sampleData from "../sample-fishes"
 
@@ -30,7 +31,12 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="Fresh Seafood Market" />
+                    <Header tagline="Fresh Food Store" />
+                    <ul className="list-of-fishes">
+                        {Object.keys(this.state.fishes).map(item => {
+                            return <Fish key={item} details={this.state.fishes[item]} />
+                        })}
+                    </ul>
                 </div>
                 <Order />
                 <Inventory addFish={this.addFish} loadSampleData={this.loadSampleData} />
